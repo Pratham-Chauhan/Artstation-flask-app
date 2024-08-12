4483584dca5d9abd5f49f6124054a657c6bbf750
Pratham Chauhan (PC) <prathamchauhan.coding42@gmail.com>
2024-08-13 03:51:23 +0530

    Image fit to screen
    pressing numpad key 0 to view the image that fit to screen (both width
    and height), little animation shown when hiding the top panel/menu.

--- static/script1.js
+++ static/script1.js
@@ -2,150 +2,188 @@ var imgX, imgY, imgH, imgW;
 var fullMode = false;
 
 function mouse_away_from_image_center(mouseX, mouseY) {
-    // calculate distance between mouse pointer and image center
-    imgX = $("#viewer")[0].offsetLeft;
-    imgY = $("#viewer")[0].offsetTop;
+  // calculate distance between mouse pointer and image center
+  imgX = $("#viewer")[0].offsetLeft;
+  imgY = $("#viewer")[0].offsetTop;
 
-    imgH = $("#viewer")[0].offsetHeight;
-    imgW = $("#viewer")[0].offsetWidth;
+  imgH = $("#viewer")[0].offsetHeight;
+  imgW = $("#viewer")[0].offsetWidth;
 
-    deltaY = mouseY - (imgY + imgH / 2);
-    deltaX = imgX + imgW / 2 - mouseX;
-    console.log("delta X Y:", deltaX, deltaY);
+  deltaY = mouseY - (imgY + imgH / 2);
+  deltaX = imgX + imgW / 2 - mouseX;
+  console.log("delta X Y:", deltaX, deltaY);
 
-    // return [deltaX, deltaY];
+  // return [deltaX, deltaY];
 }
 
 function move_image_to_center() {
-    // get image position
-    imgX = $("#viewer")[0].offsetLeft;
-    imgY = $("#viewer")[0].offsetTop;
+  // get image position
+  imgX = $("#viewer")[0].offsetLeft;
+  imgY = $("#viewer")[0].offsetTop;
 
-    imgH = $("#viewer")[0].offsetHeight;
-    imgW = $("#viewer")[0].offsetWidth;
+  imgH = $("#viewer")[0].offsetHeight;
+  imgW = $("#viewer")[0].offsetWidth;
 
-    console.log("Intial x y:", imgX, imgY);
+  console.log("Intial x y:", imgX, imgY);
 
-    // distance between the image center and the screen center coordinates
-    var dx = imgX + imgW / 2 - centerX;
-    var dy = imgY + imgH / 2 - centerY;
+  // distance between the image center and the screen center coordinates
+  var dx = imgX + imgW / 2 - centerX;
+  var dy = imgY + imgH / 2 - centerY;
 
-    console.log("dx:", dx);
-    console.log("dy:", dy);
+  console.log("dx:", dx);
+  console.log("dy:", dy);
 
-    var x2 = imgX - dx;
-    var y2 = imgY - dy;
+  var x2 = imgX - dx;
+  var y2 = imgY - dy;
 
-    console.log("final x:", x2, "screen center:", centerX, x2 + imgW / 2);
-    console.log("final y:", y2, "screen center:", centerY, y2 + imgH / 2);
+  console.log("final x:", x2, "screen center:", centerX, x2 + imgW / 2);
+  console.log("final y:", y2, "screen center:", centerY, y2 + imgH / 2);
 
-    // return [x2 + 0.2* deltaX, y2 - 0.5 *deltaY]
-    return [x2, y2];
-    // return [0,0]
+  // return [x2 + 0.2* deltaX, y2 - 0.5 *deltaY]
+  return [x2, y2];
+  // return [0,0]
 }
 
-// scroll horizontally to middle of the viewer    
+// scroll horizontally to middle of the viewer
 function scrollToMiddle(element) {
-    const scrollLeft = (element.scrollWidth - element.clientWidth) / 2;
+  const scrollLeft = (element.scrollWidth - element.clientWidth) / 2;
 
-    element.scrollTo({
-        left: scrollLeft,
-        behavior: 'smooth'
-    });
+  element.scrollTo({
+    left: scrollLeft,
+    behavior: "smooth",
+  });
 }
-
 
 function hide_menu() {
-    $("#viewer").css({ width: "auto", border: "none" }); //  reset the image size and remove the border
-    window.scrollTo(0, 0);
+  document.body.style.backgroundColor = "#242424"; // dark
 
-    // hide the above panel
-    $(".menu").css({ display: "none" });
-    $(".menu2").css({ display: "none" });
+  $("#viewer").css({
+    width: "auto",
+    border: "none",
+  }); //  reset the image size and remove the border
+  window.scrollTo(0, 0);
+
+  // hide top panel
+  // $(".menu").css({ display: "none" });
+  // $(".menu2").css({ display: "none" });
+
+  
+  // Hide top panel with custom animation
+  $(".menu").animate({
+      padding: 0,
+      height: 0,
+      opacity: 0
+    }, 250);
+  $(".menu2").animate({
+      height: 0,
+      opacity: 0
+    }, 250);
+
+}
+
+function unhide_menu() {
+  console.log("unhide menu");
+
+  
+  // show top panel
+  document.body.style.backgroundColor = "hsl(0 0% 93% / 1)"; // light
+
+  $("#viewer").css({
+    width: "100%",
+    border: "3px groove #ccc",
+    left: 0 + "px",
+    top: 0 + "px",
+  });
+
+
+  // $(".menu").css({ display: "flex" });
+  // $(".menu2").css({ display: "flex" });
+
+
+  $(".menu").css({ height: "auto", padding: "8px" });
+  $(".menu2").css({ height: "auto" });
+
+  // Restore top panel with custom animation
+  $(".menu").animate({
+      opacity: 1,
+    }, 300);
+
+  $(".menu2").animate({
+      opacity: 1,
+    }, 300);
+
+  $("body").css({ overflow: "auto" });
 }
 
 function fullview(event) {
-    var mouseX = event.clientX;
-    var mouseY = event.clientY;
+  var mouseX = event.clientX;
+  var mouseY = event.clientY;
 
-    console.log(mouseX, mouseY);
+  console.log(mouseX, mouseY);
 
-    // mouse_away_from_image_center(mouseX, mouseY)
-    // var deltaY, deltaX;
+  // mouse_away_from_image_center(mouseX, mouseY)
+  // var deltaY, deltaX;
 
-    // show or hide the viewer
-    if (!fullMode) {
-        fullMode = !fullMode;
+  // show or hide the viewer
+  if (!fullMode) {
+    fullMode = !fullMode;
 
-        hide_menu();
+    hide_menu();
 
-        document.body.style.backgroundColor = "#242424"; // dark
+    if (device_type == "desktop") {
+      // show the image relocation animation
+      const element = document.getElementById("viewer");
+      initial_pos = calculate_image_position(mouseX, mouseY);
+      middle_pos = move_image_to_center();
+      init_x = initial_pos[0];
+      init_y = initial_pos[1];
 
-        if (device_type == "desktop") {
-            // show the image relocation animation
-            const element = document.getElementById("viewer");
-            initial_pos = calculate_image_position(mouseX, mouseY);
-            middle_pos = move_image_to_center();
-            init_x = initial_pos[0];
-            init_y = initial_pos[1];
-
-            element.animate(
-                [
-                    {
-                        left: middle_pos[0] + "px",
-                        top: middle_pos[1] + "px",
-                    }, // starting position
-                    { left: init_x + "px", top: init_y + "px" }, // ending position
-                ],
-                {
-                    duration: 400, // 0.7 seconds
-                    easing: "ease-out",
-                    fill: "backwards",
-                }
-            );
-
-            // jquery version
-            // $("#viewer").animate(
-            // {
-            //     left: -500 +'px' ,
-            //     top: -500 + 'px'
-            // }, // ending position
-            // {
-            //     duration: 700, // 0.7 seconds
-            //     // easing: "ease-out",
-            // }
-            // );
-
-            $("#viewer").css({ left: init_x + "px", top: init_y + "px" });
-
-            // disable scrolling, not recommended right now as floating mode is not perfect.
-            $('body').css({ overflow: 'hidden' });
-        } else { // mobile
-            // margin: auto is the cause of all the miscalculations and image position,
-            // cause it change the left and top properties of the image and center the image.
-            $("#viewer").css({ margin: "auto" });
-            
-            var container = document.getElementById("wrapper");
-            scrollToMiddle(container)
-            
-
+      element.animate(
+        [
+          {
+            left: middle_pos[0] + "px",
+            top: middle_pos[1] + "px",
+          }, // starting position
+          { left: init_x + "px", top: init_y + "px" }, // ending position
+        ],
+        {
+          duration: 400, // 0.7 seconds
+          easing: "ease-out",
+          fill: "backwards",
         }
+      );
+
+      // jquery version
+      // $("#viewer").animate(
+      // {
+      //     left: -500 +'px' ,
+      //     top: -500 + 'px'
+      // }, // ending position
+      // {
+      //     duration: 700, // 0.7 seconds
+      //     // easing: "ease-out",
+      // }
+      // );
+
+      $("#viewer").css({ left: init_x + "px", top: init_y + "px" });
+
+      // disable scrolling, not recommended right now as floating mode is not
+      // perfect.
+      $("body").css({ overflow: "hidden" });
     } else {
-        // normal view
-        fullMode = !fullMode;
+      // mobile
+      // margin: auto is the cause of all the miscalculations and image
+      // position, cause it change the left and top properties of the image and
+      // center the image.
+      $("#viewer").css({ margin: "auto" });
 
-        $("#viewer").css({
-            width: "100%",
-            border: "3px groove #ccc",
-            left: 0 + "px",
-            top: 0 + "px",
-        });
-
-        // show above panel
-        document.body.style.backgroundColor = "hsl(0 0% 93% / 1)"; // light
-        $(".menu").css({ display: "flex" });
-        $(".menu2").css({ display: "flex" });
-
-        $("body").css({ overflow: "auto" });
+      var container = document.getElementById("wrapper");
+      scrollToMiddle(container);
     }
+  } else {
+    // normal view
+    fullMode = !fullMode;
+
+    unhide_menu();
+  }
 }
--- static/script3.js
+++ static/script3.js
@@ -1,126 +1,170 @@
-
 // update image view
 function updateImg(index) {
-    console.log(index);
-    currentIndex = index;
+  console.log(index);
+  currentIndex = index;
 
-    $("a.link").attr("href", images[index].src); // update download link
+  $("a.link").attr("href", images[index].src); // update download link
 
-    $("#viewer").css({ display: "none" }); // hide the old image
+  $("#viewer").css({ display: "none" }); // hide the old image
 
-    $("#viewer").attr("src", images[index].src); // set the new image link
+  $("#viewer").attr("src", images[index].src); // set the new image link
 
-    // wait for loading the new image
-    $("#loading").css({ display: "block" }); // show loading text
+  // wait for loading the new image
+  $("#loading").css({ display: "block" }); // show loading text
 
-    $("#viewer")[0].onload = function () {
-        $("#loading").css({ display: "none" });
-        $("#viewer").css({ display: "block" });
-        updateSize();
-    };
+  $("#viewer")[0].onload = function () {
+    $("#loading").css({ display: "none" });
+    $("#viewer").css({ display: "block" });
+    updateSize();
+  };
 
-    $(".indicator").removeClass("active");
-    $(`.indicator[data-index=${index}]`).addClass("active");
+  $(".indicator").removeClass("active");
+  $(`.indicator[data-index=${index}]`).addClass("active");
 }
 // update image size text
 function updateSize() {
-    var img = $("#viewer")[0];
-    // console.log('Image is loaded!')
-    // get image size
-    // imgH = img.offsetHeight; // height on the screen
-    // imgW = img.offsetWidth; // width on the screen
+  var img = $("#viewer")[0];
+  // console.log('Image is loaded!')
+  // get image size
 
-    i_height = img.naturalHeight;
-    i_width = img.naturalWidth;
+  i_height = img.naturalHeight;
+  i_width = img.naturalWidth;
 
-    ratex = (i_width / window.innerWidth).toFixed(1);
-    ratey = (i_height / window.innerHeight).toFixed(1);
+  ratex = (i_width / window.innerWidth).toFixed(1);
+  ratey = (i_height / window.innerHeight).toFixed(1);
 
-    $("#size_info").html(`${i_width} x ${i_height}`);
+  $("#size_info").html(`${i_width} x ${i_height}`);
 }
 
 if ($("#viewer")[0].complete) {
-    updateSize();
+  updateSize();
 } else {
-    $("#viewer")[0].onload = updateSize;
+  $("#viewer")[0].onload = updateSize;
 }
 
 // indicator menu
 $.each(imageUrls, function (index) {
-    // const button = $('<button class="button">').text(`${index}`)
-    const indicator = $('<div class="indicator">').attr("data-index", index);
-    indicator.click(() => updateImg(index));
+  // const button = $('<button class="button">').text(`${index}`)
+  const indicator = $('<div class="indicator">').attr("data-index", index);
+  indicator.click(() => updateImg(index));
 
-    $(".menu").append(indicator);
+  $(".menu").append(indicator);
 });
 
 var currentIndex = 0;
 updateImg(currentIndex); // show the first image
 
 function goToNextImage() {
-    updateImg((currentIndex + 1) % imageUrls.length);
+  updateImg((currentIndex + 1) % imageUrls.length);
 }
 function goToPrevImage() {
-    updateImg((currentIndex - 1 + imageUrls.length) % imageUrls.length);
+  updateImg((currentIndex - 1 + imageUrls.length) % imageUrls.length);
 }
 // Event Listeners -- Interactive
 document.addEventListener("click", (event) => {
-    const width = window.innerWidth;
-    const clickPosition = event.clientX;
+  const width = window.innerWidth;
+  const clickPosition = event.clientX;
 
-    var threshold = window.innerWidth * 0.1; // 10% of the window width
-    if (device_type == "mobile") {
-        threshold = window.innerWidth * 0.16; // 20% of the window width (mobile)
+  var threshold = window.innerWidth * 0.1; // 10% of the window width
+  if (device_type == "mobile") {
+    threshold = window.innerWidth * 0.16; // 20% of the window width (mobile)
+  }
+  console.log("Clicked on", event.target.id, clickPosition, width);
+
+  if (clickPosition < threshold) {
+    // Clicked on the left side
+    goToPrevImage();
+  } else if (clickPosition > window.innerWidth - threshold) {
+    // Clicked on the right side
+    goToNextImage();
+  } else {
+    console.log("clicked in between screen.");
+    if (event.target.id == "viewer") {
+      fullview(event);
     }
-    console.log("Clicked on", event.target.id, clickPosition, width);
-
-    if (clickPosition < threshold) {// Clicked on the left side
-        goToPrevImage();
-    } else if (clickPosition > window.innerWidth - threshold) {// Clicked on the right side
-        goToNextImage();
-
-    } else {
-        console.log("clicked in between screen.");
-        if (event.target.id == "viewer") {
-            fullview(event);
-        }
-    }
+  }
 });
 
+let isKeyDown = false;
 document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowLeft") {
-        goToPrevImage();
-    } else if (event.key === "ArrowRight") {
-        goToNextImage();
+  if (event.key === "ArrowLeft") {
+    goToPrevImage();
+  } else if (event.key === "ArrowRight") {
+    goToNextImage();
+  } else if (event.key === "0") {
+
+      if (!isKeyDown) {
+        isKeyDown = true;
+        console.log("Fit to Screen");
+        hide_menu();
+    
+        $("#wrapper").css({
+          height: "100vh",
+          width: "100vw",
+        });
+    
+        $("#viewer").css({
+          "pointer-events": "none",
+          "max-width": "100%",
+          "max-height": "100%",
+          margin: "auto",
+        });
+        $("body").css({ overflow: "hidden" });
+        
     }
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "0") {
+    isKeyDown = false;
+
+    if (!fullMode) {
+      unhide_menu();
+      $("body").css({ overflow: "" });
+    }
+
+    $("#wrapper").css({
+      height: "auto",
+      width: "auto",
+    });
+
+    $("#viewer").css({
+      "pointer-events": "",
+      "max-width": "",
+      "max-height": "",
+      margin: "",
+    });
+  }
 });
 
 // HD mode
 let hdMode = false;
 document.addEventListener("contextmenu", (event) => {
-    if (event.target.id == "viewer") {
-        event.preventDefault(); // Prevent the default context menu from appearing
-        const img = event.target;
-        const largeSrc = img.src.replace("/4k/", "/large/");
-        const hdSrc = img.src.replace("/large/", "/4k/");
+  if (event.target.id == "viewer") {
+    event.preventDefault(); // Prevent the default context menu from appearing
+    const img = event.target;
+    const largeSrc = img.src.replace("/4k/", "/large/");
+    const hdSrc = img.src.replace("/large/", "/4k/");
 
-        if (hdMode) {
-            event.target.src = largeSrc;
-        } else {
-            event.target.src = hdSrc;
-        }
-
-        hdMode = !hdMode;
+    if (hdMode) {
+      event.target.src = largeSrc;
+    } else {
+      event.target.src = hdSrc;
     }
+
+    hdMode = !hdMode;
+  }
 });
 
-window.addEventListener('wheel', (event) => {
-    if (fullMode) {
-        if (event.deltaY < 0) { // Scrolling up
-            goToNextImage();
-
-        } else if (event.deltaY > 0) { // Scrolling down
-            goToPrevImage();
-        }
+window.addEventListener("wheel", (event) => {
+  if (fullMode) {
+    if (event.deltaY < 0) {
+      // Scrolling up
+      goToNextImage();
+    } else if (event.deltaY > 0) {
+      // Scrolling down
+      goToPrevImage();
     }
+  }
 });
